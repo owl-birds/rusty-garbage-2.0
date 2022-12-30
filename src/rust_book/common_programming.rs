d4b741cffff75b3c2677a11cbbbf8346f4993205
ooowwwlll <75400842+owl-birds@users.noreply.github.com>
2022-12-30 23:55:45 +0700

    ownership

--- src/rust_book/common_programming.rs
+++ src/rust_book/common_programming.rs
@@ -1,3 +1,21 @@
+pub fn fahrenheit_to_celcius(temp: f64)->f64{
+    return (temp-32.0) * 5.0/9.0;
+}
+pub fn fib_number_at(at: i64)->i64{
+    if at == 0{
+        return 0;
+    }
+    let mut prev_num = 0;
+    let mut curr_num = 1;
+    let mut curr_at = 1;
+    while curr_at < at{
+        let temp_num = curr_num;
+        curr_num = curr_num + prev_num;
+        prev_num = temp_num;
+        curr_at += 1;
+    }
+    return curr_num;
+}
 pub fn common_progamming_concepts(){
     //variables are immutable by default
     println!("COMMON PROGRAMMING CONCEPTS");
@@ -40,5 +58,35 @@ pub fn common_progamming_concepts(){
         }
     };
     println!("{result}");
+    
     // loop labels to disambiguate between multiple loops
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count: {count}");
+        let mut remaining = 10;
+        loop {
+            println!("remaining : {remaining}");
+            if remaining == 9{
+                break;
+            }
+            if count == 2{
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    }
+    println!("End count : {count}");
+    let temp_arr = [1,2,3,4,5];
+    for element in temp_arr{
+        println!("value is : {element}");
+    }
+    for number in (1..5).rev(){
+        println!("reverse : {number}");
+    }
+    let temperature = fahrenheit_to_celcius(129.0);
+    println!("129 f = {temperature} c");
+    let fib_num = fib_number_at(12);
+    println!("fib num at 12 : {fib_num}");
 }
+
